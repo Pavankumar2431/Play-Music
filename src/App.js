069bb8cfdcfb5fee0c89df0a4215d0e5a6f7b2c6
Pavankumar2431 <mallipavankumar99@gmail.com>
2024-09-21 11:37:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [keyword, setKeyword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [tracks, setTracks] = useState([]);
+
+  const getTracks = async () => {
+    setIsLoading(true);
+    let data = await fetch(
+      `https://v1.nocodeapi.com/pavan24/spotify/WUniVkXekuzfLdHQ/search?q=${
+        keyword === "" ? "trending" : keyword
+      }&type=track`
+    );
+    let convertedData = await data.json();
+    setTracks(convertedData.tracks.items);
+    setIsLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar navbar-dark navbar-expand-lg bg-success">
+        <div className="container-fluid">
+            <span className="bi bi-music-note-list mx-3 navbar-brand ml-auto">Play music</span> 
+          <div
+            className="collapse navbar-collapse d-flex justify-content-center"
+            id="navbarSupportedContent"
+          >
+            <input
+              value={keyword}
+              onChange={(event) => setKeyword(event.target.value)}
+              className="form-control me-2 w-75"
+              type="search"
+              placeholder="Search for a song or album"
+              aria-label="Search"
+            />
+            <button onClick={getTracks} className="btn btn-outline-light">
+              Search
+            </button>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container">
+        <div className={`row ${isLoading ? "" : "d-none"}`}>
+          <div className="col-12 py-5 text-center">
+            <div
+              className="spinner-border"
+              style={{ width: "48px", height: "48px" }}
+              role="status"
+            >
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        </div>
+        <div className={`row ${tracks.length === 0 ? "" : "d-none"}`}>
+          <div className="col-12 py-5 text-center">
+            <h1>
+              <i className="bi bi-music-note-list  text-center pt-5"></i>
+              Welcome to Play Music
+            </h1>
+            <h3 className="pt-5 text-center">Search for your favorite music above.</h3>
+          </div>
+        </div>
+        <div className="row">
+          {tracks.map((element) => {
+            return (
+              <div key={element.id} className="col-lg-3 col-md-6 py-2">
+                <div className="card">
+                  <div className="ratio ratio-1x1 bg-secondary bg-opacity-25">
+                    <img
+                      src={element.album.images[0].url}
+                      className="card-img-top"
+                      alt="..."
+                    />
+                  </div>
+                  <div className="card-body">
+                    <h5 className="card-title">{element.name}</h5>
+                    <p className="card-text">
+                      Artist: {element.album.artists[0].name}
+                    </p>
+                    <p className="card-text">
+                      Release date: {element.album.release_date}
+                    </p>
+                    <audio
+                      src={element.preview_url}
+                      controls
+                      className="w-100"
+                    ></audio>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
